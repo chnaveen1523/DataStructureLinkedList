3fd51ca9816d86142b9b5ced83cd9816bd5957fd
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-24 09:49:38 +0530

    [Naveen] Added a Appending Code

--- Program.cs
+++ Program.cs
@@ -16,9 +16,9 @@ namespace DataStructureLinkedList.cs
             //list.AddLast(70);
 
             CustomLinkedList customLinkedList = new CustomLinkedList();
-            customLinkedList.InsertFront(70);
-            customLinkedList.InsertFront(30);
-            customLinkedList.InsertFront(56);
+            customLinkedList.Append(56);
+            customLinkedList.Append(30);
+            customLinkedList.Append(70);
             customLinkedList.Display();
             Console.ReadLine();
         }
