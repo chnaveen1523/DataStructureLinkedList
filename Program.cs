6844142f90ea4015ba26542d5987c86af365f43b
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-24 09:44:22 +0530

    [Naveen] Added a code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList < int > list= new LinkedList<int>();
+            //list.AddLast(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
+
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.InsertLast(56);
+            customLinkedList.InsertLast(30);
+            customLinkedList.InsertLast(70);
+            Console.WriteLine("Linked list before insertion:");
+            customLinkedList.Display();
+            customLinkedList.FindAndInsert(customLinkedList.head.next, 40);
+            customLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
