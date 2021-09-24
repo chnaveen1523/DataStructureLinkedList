3d88eb6fd4b41065830ffd7d4ad74936f5a54c18
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-24 09:56:44 +0530

    [Naveen] Added a Find Node Code

--- Program.cs
+++ Program.cs
@@ -19,11 +19,7 @@ namespace DataStructureLinkedList.cs
             customLinkedList.InsertLast(56);
             customLinkedList.InsertLast(30);
             customLinkedList.InsertLast(70);
-            Console.WriteLine("Linked list before deletion:");
-            customLinkedList.Display();
-            customLinkedList.DeleteLastNode();
-            Console.WriteLine("Linked list after deletion:");
-            customLinkedList.Display();
+            Console.WriteLine(customLinkedList.Search(30));
             Console.ReadLine();
         }
     }
